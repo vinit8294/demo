1f1727a17284234f6e8620e444df1637056dcc2a
vinit <vinit8986@gmail.com>
2024-03-21 12:42:30 +0530

    my commit

---
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public void msg(View view){
+        Toast.makeText(this, "welcome to my first app..", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
